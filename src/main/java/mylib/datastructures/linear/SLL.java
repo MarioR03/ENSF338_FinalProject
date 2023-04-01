4ba35ca66fbf1cdb460af765b08fc44906178b78
MarioR03 <mario.rodriguez@ucalgary.ca>
2023-04-01 15:49:57 -0600

    finished and tested SLL, everything worked perfectly

--- src/main/java/mylib/datastructures/linear/SLL.java
+++ src/main/java/mylib/datastructures/linear/SLL.java
@@ -3,51 +3,29 @@ package main.java.mylib.datastructures.linear;
 import main.java.mylib.datastructures.nodes.DNode;
 
 public class SLL {
-    private DNode head;
-    private int counter;
+    DNode head;
+    DNode tail;
+    int counter;
 
     // Contructors
     public SLL() {
         this.head = null;
+        this.tail = null;
         this.counter = 0;
     }
 
     public SLL(DNode node) {
         this.head = node;
+        this.tail = node;
         this.counter = 1;
     }
 
     // Insertions
-    public void InsertTail(DNode node) {
-        if (this.counter == 0) {
-            this.head = node;
-        } else {
-            DNode last = this.head;
-            while (last.getNext() != null) {
-                last = last.getNext();
-            }
-            last.setNext(node);
-        }
-        this.counter++;
-    }
-
-    public void InsertTail(int data) {
-        DNode node = new DNode(data);
-        if (this.counter == 0) {
-            this.head = node;
-        } else {
-            DNode last = this.head;
-            while (last.getNext() != null) {
-                last = last.getNext();
-            }
-            last.setNext(node);
-        }
-        this.counter++;
-    }
-
     public void InsertHead(DNode node) {
+        // If the list is empty
         if (this.counter == 0) {
             this.head = node;
+            this.tail = node;
         } else {
             node.setNext(this.head);
             this.head = node;
@@ -55,119 +33,96 @@ public class SLL {
         this.counter++;
     }
 
-    public void InsertHead(int data) {
-        DNode node = new DNode(data);
+    public void InsertTail(DNode node) {
+        //If the list is empty
         if (this.counter == 0) {
             this.head = node;
+            this.tail = node;
+            //else the list is not empty
         } else {
+            this.tail.setNext(node);
+            this.tail = node;
+        }
+        this.counter++;
+    }
+
+    public void insert(DNode node, int position) throws IndexOutOfBoundsException {
+        if (position < 0 || position > this.counter) {
+            throw new IndexOutOfBoundsException();
+        }
+    
+        if (position == 0) { // inserting at the beginning of the list
             node.setNext(this.head);
             this.head = node;
-        }
-        this.counter++;
-    }
-
-    public void Insert(DNode node, int index) throws IndexOutOfBoundsException { // Can they put something at position
-                                                                                   // zero?
-        if (index < 0 || index > this.counter) { // Incase they try to put a node in a position further out than the
-                                                 // last
-            // position
-            throw new IndexOutOfBoundsException();
-        }
-
-        DNode temp = this.head;
-
-        for (int i = 0; i < index - 1; i++) {
-            temp = temp.getNext();
-        }
-        node.setNext(temp.getNext());
-        temp.setNext(node);
-        this.counter++;
-    }
-
-    public void Insert(int data, int index) throws IndexOutOfBoundsException {
-        if (index < 0 || index > this.counter) { // Incase they try to put a node in a position further out than the
-                                                 // last
-            // position
-            throw new IndexOutOfBoundsException();
-        }
-
-        DNode node = new DNode(data);
-        DNode temp = this.head;
-
-        for (int i = 0; i < index - 1; i++) {
-            temp = temp.getNext();
-        }
-        node.setNext(temp.getNext());
-        temp.setNext(node);
-        this.counter++;
-    }
-
-    // Deletions
-    public void deleteHead() {
-        // If the list is empty, do nothing
-        if (this.head == null) {
-            return;
-        }
-        // If the list has only one element, set head to null
-        if (this.head.getNext() == null) {
-            this.head = null;
-            this.counter = 0;
-            return;
-        } else {
-            this.head = this.head.getNext();
-            this.counter--;
-        }
-    }
-
-    public void deleteTail() {
-        // If the list is empty, do nothing
-        if (this.head == null) {
-            return;
-        }
-        // If the list has only one element, set head to null
-        if (this.head.getNext() == null) {
-            this.head = null;
-            this.counter = 0;
-            return;
-        }
-        // Otherwisem iterate thriugh the list to find the second-to-last node
-        DNode temp = this.head;
-        for (int i = 0; i < this.counter - 2; i++) {
-            temp = temp.getNext();
-        }
-        // Set the next pointer of the second-to-last node to null
-        temp.setNext(null);
-        // Decrement the counter
-        this.counter--;
-    }
-
-    public void Delete(DNode node) {
-        // Check if the node is the head
-        if (this.head.getData() == node.getData()) {
-            this.head = this.head.getNext();
-            this.counter--;
-            return;
-        } else {// check anywhere else
-            DNode prevNode = this.head;
-            for (int i = 0; i < this.counter; i++) {
-                DNode current = prevNode.getNext();
-                if (current.getData() == node.getData()) {
-                    prevNode.setNext(current.getNext());
-                    this.counter--;
-                    return;
-                }
-                prevNode = prevNode.getNext();
+            if (this.counter == 0) {
+                // the list was empty before, so update the tail pointer
+                this.tail = node;
             }
-            return;
+        } else if (position == this.counter) { // inserting at the end of the list
+            if (this.counter == 0) {
+                // the list was empty before, so set both head and tail to the new node
+                this.head = node;
+                this.tail = node;
+            } else {
+                // update the next pointer of the current tail node to point to the new node
+                this.tail.setNext(node);
+                // update the tail pointer to the new node
+                this.tail = node;
+            }
+        } else { // inserting in the middle of the list
+            DNode temp = this.head;
+            for (int i = 0; i < position - 1; i++) {
+                temp = temp.getNext();
+            }
+            node.setNext(temp.getNext());
+            temp.setNext(node);
         }
+        this.counter++;
     }
+    
+    public void SortedInsert(DNode node) {
 
-    // THIS IS TO CLEAR THE WHOLE LL
-    public void Clear() {
-        // Set the head pointer to null, effectively removing all nodes from the list
-        this.head = null;
-        // Reset the counter to zero to indicate an empty list
-        this.counter = 0;
+        if (!this.isSorted()){
+            this.Sort();
+        }
+    
+        // Check if the list is empty or the new node's data is less than the head
+        if (this.head == null || node.getData() < this.head.getData()) {
+            node.setNext(this.head);
+            this.head = node;
+            if (this.tail == null) {
+                this.tail = node;
+            }
+        } else {
+            // Find the node in the list after which the new node should be inserted
+            DNode temp = this.head;
+            while (temp.getNext() != null && temp.getNext().getData() <= node.getData()) {
+                temp = temp.getNext();
+            }
+            // Insert the new node after the found node
+            node.setNext(temp.getNext());
+            temp.setNext(node);
+            if (node.getNext() == null) {
+                this.tail = node;
+            }
+        }
+        this.counter++;
+    }
+    
+
+    //helper function
+    private boolean isSorted(){
+        if (this.head == null || this.head.getNext() == null){
+            return true;
+        }
+        DNode current = this.head;
+        while(current.getNext() != null){
+            if (current.getData() > current.getNext().getData()){
+                return false;
+            }
+            current = current.getNext();
+        }
+        return true;
     }
 
     public DNode Search(DNode node) {
@@ -181,13 +136,96 @@ public class SLL {
         }
         return null;
     }
+    
+    // Deletions
+    public void DeleteHead() {
+        // If the list is empty, do nothing
+        if (this.head == null) {
+            return;
+        }
+        // If the list has only one element, set head to null
+        if (this.head.getNext() == null) {
+            this.head = null;
+            this.tail = null;
+            this.counter = 0;
+            return;
+        } else {
+            this.head = this.head.getNext();
+            this.counter--;
+        }
+    }
+
+    public void DeleteTail() {
+        // If the list is empty, do nothing
+        if (this.head == null) {
+            return;
+        }
+        // If the list has only one element, set head and tail to null
+        if (this.head.getNext() == null) {
+            this.head = null;
+            this.tail = null;
+            this.counter = 0;
+            return;
+        }
+        // Otherwise, iterate through the list to find the second-to-last node
+        DNode temp = this.head;
+        while (temp.getNext() != this.tail) {
+            temp = temp.getNext();
+        }
+        // Set the tail pointer to the second-to-last node
+        this.tail = temp;
+        // Set the next pointer of the second-to-last node to null
+        temp.setNext(null);
+        // Decrement the counter
+        this.counter--;
+    }
+    
+
+    public void Delete(DNode node) {
+        // Check if the node is the head
+        if (this.head.getData() == node.getData()) {
+            this.head = this.head.getNext();
+            this.counter--;
+            // If the list has only one element, update tail to null
+            if (this.head == null) {
+                this.tail = null;
+            }
+            return;
+        }
+    
+        // Check if the node is the tail
+        if (this.tail.getData() == node.getData()) {
+            // Find the second-to-last node
+            DNode temp = this.head;
+            for (int i = 0; i < this.counter - 2; i++) {
+                temp = temp.getNext();
+            }
+            temp.setNext(null);
+            this.tail = temp;
+            this.counter--;
+            return;
+        }
+    
+        // Check anywhere else in the list
+        DNode prevNode = this.head;
+        for (int i = 0; i < this.counter - 1; i++) {
+            DNode current = prevNode.getNext();
+            if (current.getData() == node.getData()) {
+                prevNode.setNext(current.getNext());
+                this.counter--;
+                return;
+            }
+            prevNode = prevNode.getNext();
+        }
+    }
+    
 
     public void Sort() {
         // If the list is empty or has only one node, it is already sorted
-        if (this.head == null || this.head.getNext() == null) {
+        if (this.head == null || this.head.getNext() == null || this.isSorted()) {
             return;
         }
-
+    
         DNode current = this.head.getNext();
         DNode prev = this.head;
         while (current != null) {
@@ -195,7 +233,26 @@ public class SLL {
                 // Remove the current node from the list
                 prev.setNext(current.getNext());
                 // Insert the current node at the right position
-                this.SortedInsert(current);
+    
+                if (current.getData() < this.head.getData()) {
+                    current.setNext(this.head);
+                    this.head = current;
+                } else {
+                    // Find the node in the list after which the new node should be inserted
+                    DNode temp = this.head;
+                    while (temp.getNext() != null && temp.getNext().getData() <= current.getData()) {
+                        temp = temp.getNext();
+                    }
+                    // Insert the new node after the found node
+                    current.setNext(temp.getNext());
+                    temp.setNext(current);
+                }
+    
+                // Update the tail pointer if necessary
+                if (prev.getNext() == null) {
+                    this.tail = prev;
+                }
+    
                 // Update the current node to the next node
                 current = prev.getNext();
             } else {
@@ -204,51 +261,35 @@ public class SLL {
                 current = current.getNext();
             }
         }
-
+    
         // Set the head of the list to the first node
-        DNode newHead = this.head.getNext();
-        this.head.setNext(null);
-        this.head = newHead;
+        // DNode newHead = this.head.getNext();
+        // this.head.setNext(null);
+        // this.head = newHead;
+    }    
+
+    // THIS IS TO CLEAR THE WHOLE LL
+    public void Clear() {
+        // Set the head pointer to null, effectively removing all nodes from the list
+        this.head = null;
+        this.tail = null;
+        // Reset the counter to zero to indicate an empty list
+        this.counter = 0;
     }
 
-    public void SortedInsert(DNode node) {
-        // Check if the list is empty or the new node's data is less than the head
-        if (this.head == null || node.getData() < this.head.getData()) {
-            node.setNext(this.head);
-            this.head = node;
-        } else {
-            // Find the node in the list after which the new node should be inserted
-            DNode temp = this.head;
-            while (temp.getNext() != null && temp.getNext().getData() <= node.getData()) {
-                temp = temp.getNext();
-            }
-            // Insert the new node after the found node
-            node.setNext(temp.getNext());
-            temp.setNext(node);
-        }
-        this.counter++;
-    }
     public void Print() {
         // Print the list length
         System.out.println("List length: " + this.counter);
     
         // Determine the sorted status of the list
-        boolean sorted = true;
-        DNode current = this.head;
-        while (current != null && current.getNext() != null) {
-            if (current.getData() > current.getNext().getData()) {
-                sorted = false;
-                break;
-            }
-            current = current.getNext();
-        }
-    
+        boolean sorted = this.isSorted();
+
         // Print the sorted status
         System.out.println("Sorted: " + (sorted ? "Yes" : "No"));
     
         // Print the list content
         System.out.print("List content: ");
-        current = this.head;
+        DNode current = this.head;
         while (current != null) {
             System.out.print(current.getData() + " ");
             current = current.getNext();
