6b0c935e209a8fe4284dd031450e37b94664d7bf
MarioR03 <mario.rodriguez@ucalgary.ca>
2023-04-12 20:54:18 -0600

    added the queue and stack tests

--- src/main/java/mylib/datastructures/linear/QueueLL.java
+++ src/main/java/mylib/datastructures/linear/QueueLL.java
@@ -5,71 +5,83 @@ import main.java.mylib.datastructures.nodes.DNode;
 /**
  * Represents a Linked List based Queue
  */
-public class QueueLL extends SLL{
+public class QueueLL extends SLL {
     /**
      * Default constructor that creates an empty QueueLL
      */
-    public QueueLL(){
+    public QueueLL() {
         super();
     }
+
     /**
      * Default constructor that creates a QueueLL with one object
+     * 
      * @param node a DNode object you wish to insert into the QueueLL
      */
-    public QueueLL(DNode node){
+    public QueueLL(DNode node) {
         super(node);
     }
+
     /**
      * Inserts the given node at the tail of the queue.
      * 
      * @param node the node to enqueue
      */
-    public void enqueue(DNode node){
+    public void enqueue(DNode node) {
         super.InsertTail(node);
     }
+
     /**
      * dequeue
+     * 
      * @return - node that was dequeued
      */
-    public DNode dequeue(){
+    public DNode dequeue() {
 
         DNode node = this.head;
         super.DeleteHead();
         return node;
     }
+
     /**
      * peek
+     * 
      * @return - head of the queue
      */
-    public DNode peek(){
+    public DNode peek() {
         return this.head;
     }
+
     /**
      * search
+     * 
      * @param node - DNode object you are looking for
      * @return - value that is to be searched for in queue, -1 if not found
      */
-    public int search(DNode node){
+    public int search(DNode node) {
         DNode current = this.head;
-        for(int i=0; i < this.counter; i++){
-            if(current != null && node.getData() == current.getData()){
+        for (int i = 0; i < this.counter; i++) {
+            if (current != null && node.getData() == current.getData()) {
                 return i;
             }
             current = current.getNext();
         }
         return -1;
     }
+
     /**
      * empty
+     * 
      * @return - boolean value that tells you if the list is empty or not
      */
-    public boolean empty(){
+    public boolean empty() {
         return this.head == null;
     }
+
     /**
      * Deletes every element in the QueueLL and leaves you with an empty QueueLL
      */
-    public void clear(){
+    public void clear() {
         super.Clear();
     }
 
@@ -77,40 +89,116 @@ public class QueueLL extends SLL{
      * Does nothing as this is not a proper method for a QueueLL
      */
     @Override
-    public void InsertHead(DNode node) {}
+    public void InsertHead(DNode node) {
+    }
+
     /**
      * Does nothing as this is not a proper method for a QueueLL
      */
     @Override
-    public void InsertTail(DNode node) {}
+    public void InsertTail(DNode node) {
+    }
+
     /**
      * Does nothing as this is not a proper method for a QueueLL
      */
     @Override
-    public void Insert(DNode node, int position){}
+    public void Insert(DNode node, int position) {
+    }
+
     /**
      * Does nothing as this is not a proper method for a QueueLL
      */
     @Override
-    public void SortedInsert(DNode node) {}
+    public void SortedInsert(DNode node) {
+    }
+
     /**
      * Does nothing as this is not a proper method for a QueueLL
      */
     @Override
-    public void DeleteHead() {}
+    public void DeleteHead() {
+    }
+
     /**
      * Does nothing as this is not a proper method for a QueueLL
      */
     @Override
-    public void DeleteTail() {}
+    public void DeleteTail() {
+    }
+
     /**
      * Does nothing as this is not a proper method for a QueueLL
      */
     @Override
-    public void Delete(DNode node) {}
+    public void Delete(DNode node) {
+    }
+
     /**
      * Does nothing as this is not a proper method for a QueueLL
      */
     @Override
-    public void Sort() {}
+    public void Sort() {
+    }
+
+    public static void main(String[] args) {
+        // Create an instance of the class
+        QueueLL queue = new QueueLL();
+
+        // Testing out enqueue
+        queue.enqueue(new DNode(1));
+        queue.enqueue(new DNode(2));
+        queue.enqueue(new DNode(0));
+        System.out.println("\n-----Testing out enqueue method insertion-----");
+        queue.Print();
+
+        // Testing out the dequeue return value
+        DNode node = queue.dequeue();
+        System.out.println("\n-----Testing out dequeue method return-----");
+        queue.Print();
+        System.out.println("Dequeued data: " + node.getData());
+
+        // Dequeue until the Queue is empty
+        queue.dequeue();
+        queue.dequeue();
+        queue.dequeue();
+        System.out.println("\n-----Testing the dequeue method to clear the queue");
+        queue.Print();
+
+        // Testing out the second constructor
+        queue = new QueueLL(new DNode(10));
+        System.out.println("\n-----Testing the second constructor for the queue-----");
+        queue.Print();
+
+        // Testing the peek method after a few enqueues
+        queue.enqueue(new DNode(5));
+        queue.enqueue(new DNode(100));
+        System.out.println("\n-----Testing the peek method for the queue-----");
+        queue.Print();
+        System.out.println("Peeked data: " + queue.peek().getData());
+
+        // Searching for a node that exists
+        int position = queue.search(new DNode(5));
+        System.out.println("\n-----Searching for the position of the node with 5 as data (exists)-----");
+        System.out.println("position: " + position);
+
+        // Searching for a node that does not exit
+        position = queue.search(new DNode(9));
+        System.out.println("\n-----Searching for the position of the node with 9 as data (does not exist)-----");
+        System.out.println("position: " + position);
+
+        // Testing the clear method
+        queue.clear();
+        System.out.println("\n-----Testing the clear method for the queue-----");
+        queue.Print();
+
+        // Test the empty method with an empty queue
+        System.out.println("\n-----Testing the empty method for the empty queue-----");
+        System.out.println("empty? " + (queue.empty() ? "yes" : "no"));
+
+        // Test the empty method with an non empty queue
+        queue.enqueue(new DNode(50));
+        System.out.println("\n-----Testing the empty method for the non empty queue-----");
+        System.out.println("empty? " + (queue.empty() ? "yes" : "no"));
+    }
 }
--- src/main/java/mylib/datastructures/linear/StackLL.java
+++ src/main/java/mylib/datastructures/linear/StackLL.java
@@ -10,18 +10,21 @@ public class StackLL extends SLL {
     public StackLL() {
         super();
     }
+
     /**
      * Default constructor that creates a StackLL with one object
+     * 
      * @param node a DNode object you wish to insert into the SLL
      */
-    public StackLL(DNode node){
+    public StackLL(DNode node) {
         super(node);
     }
 
     /**
      * push
+     * 
      * @param node - DNode object you wish to push to the top of the stack
-     * pushes an object to the top of stack
+     *             pushes an object to the top of stack
      */
     public void push(DNode node) {
         super.InsertHead(node);
@@ -29,8 +32,9 @@ public class StackLL extends SLL {
 
     /**
      * pop
+     * 
      * @return - DNode object previously at the top of the stack
-     * deletes the the object at the top of the stack and returns it
+     *         deletes the the object at the top of the stack and returns it
      */
     public DNode pop() {
         DNode popDNode = this.head;
@@ -41,8 +45,9 @@ public class StackLL extends SLL {
 
     /**
      * peek
+     * 
      * @return - Object at the top of the stack
-     * returns the object at the top of the stack without deletion
+     *         returns the object at the top of the stack without deletion
      */
     public DNode peek() {
         return this.head;
@@ -50,14 +55,17 @@ public class StackLL extends SLL {
 
     /**
      * search
+     * 
      * @param node - DNode object you wish to find
-     * @return - The position at which the object is located on the stack, if the object is not on the stack, returns -1
-     * Searches through the StackLL and returns the location of the DNode wanted
+     * @return - The position at which the object is located on the stack, if the
+     *         object is not on the stack, returns -1
+     *         Searches through the StackLL and returns the location of the DNode
+     *         wanted
      */
-    public int search(DNode node){
+    public int search(DNode node) {
         DNode current = this.head;
-        for(int i=0; i < this.counter; i++){
-            if(current != null && node.getData() == current.getData()){
+        for (int i = 0; i < this.counter; i++) {
+            if (current != null && node.getData() == current.getData()) {
                 return i;
             }
             current = current.getNext();
@@ -67,58 +75,137 @@ public class StackLL extends SLL {
 
     /**
      * empty
+     * 
      * @return - Boolean value representing if the StackLL is empty or not
-     * Checks if Stack is empty
+     *         Checks if Stack is empty
      */
-    public boolean empty(){
+    public boolean empty() {
         return this.head == null;
     }
 
     /**
      * clear
-     * Deletes every element in the StackLL and now it leaves you with an empty stack
+     * Deletes every element in the StackLL and now it leaves you with an empty
+     * stack
      */
-    public void clear(){
+    public void clear() {
         super.Clear();
     }
+
     /**
      * Does nothing as this is not a proper method for a QueueLL
      */
     @Override
-    public void InsertHead(DNode node) {}
+    public void InsertHead(DNode node) {
+    }
+
     /**
      * Does nothing as this is not a proper method for a QueueLL
      */
     @Override
-    public void InsertTail(DNode node){}
+    public void InsertTail(DNode node) {
+    }
+
     /**
      * Does nothing as this is not a proper method for a QueueLL
      */
     @Override
-    public void Insert(DNode node, int position){}
+    public void Insert(DNode node, int position) {
+    }
+
     /**
      * Does nothing as this is not a proper method for a QueueLL
      */
     @Override
-    public void SortedInsert(DNode node) {}
+    public void SortedInsert(DNode node) {
+    }
+
     /**
      * Does nothing as this is not a proper method for a QueueLL
      */
     @Override
-    public void DeleteHead() {}
+    public void DeleteHead() {
+    }
+
     /**
      * Does nothing as this is not a proper method for a QueueLL
      */
     @Override
-    public void DeleteTail() {}
+    public void DeleteTail() {
+    }
+
     /**
      * Does nothing as this is not a proper method for a QueueLL
      */
     @Override
-    public void Delete(DNode node) {}
+    public void Delete(DNode node) {
+    }
+
     /**
      * Does nothing as this is not a proper method for a QueueLL
      */
     @Override
-    public void Sort() {}
+    public void Sort() {
+    }
+
+    public static void main(String[] args) {
+        // Create an instance of the class
+        StackLL stack = new StackLL();
+
+        // Testing push method
+        stack.push(new DNode(1));
+        stack.push(new DNode(2));
+        stack.push(new DNode(0));
+        System.out.println("\n-----Testing out push method insertion-----");
+        stack.Print();
+
+        // Testing out the pop method
+        DNode num = stack.pop();;
+        System.out.println("\n-----Testing out pop method-----");
+        stack.Print();
+        System.out.println("Popped data: " + num.getData());
+
+        // Pop until the stack is empty
+        stack.pop();
+        stack.pop();
+        stack.pop();
+        System.out.println("\n-----Testing out pop method to clear the stack-----");
+        stack.Print();
+
+        // Testing out the second constructor
+        stack = new StackLL(new DNode(10));
+        System.out.println("\n-----Testing the second constructor for the stack-----");
+        stack.Print();
+
+        // Testing the peek method after a few pushes
+        stack.push(new DNode(5));
+        stack.push(new DNode(100));
+        System.out.println("\n-----Testing the peek method for the stack-----");
+        stack.Print();
+        System.out.println("Peeked data: " + stack.peek().getData());
+
+        // Searching for a node that exists
+        int position = stack.search(new DNode(10));
+        System.out.println("\n-----Searching for the position of the node with 10 as data (exists)-----");
+        System.out.println("position: " + position);
+
+        // Searching for a node that does not exit
+        position = stack.search(new DNode(9));
+        System.out.println("\n-----Searching for the position of the node with 9 as data (does not exist)-----");
+        System.out.println("position: " + position);
+
+        // Testing the clear method
+        stack.clear();
+        System.out.println("\n-----Testing the clear method for the stack-----");
+        stack.Print();
+
+        // Test the empty method with an empty stack
+        System.out.println("\n-----Testing the empty method for the empty stack-----");
+        System.out.println("empty? " + (stack.empty() ? "yes":"no"));
+
+        // Test the empty method with an non empty stack
+        stack.push(new DNode(50));
+        System.out.println("\n-----Testing the empty method for the non empty stack-----");
+        System.out.println("empty? " + (stack.empty() ? "yes":"no"));
+    }
 }
